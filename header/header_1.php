337466b16ef939ebc1936d1b2c614c6dfc0e361f
uxwebb <perolov.nas@oas.nu>
2023-05-02 16:43:53 +0200

    header mobile menu

--- header/header_1.php
+++ header/header_1.php
@@ -1,10 +1,18 @@
 <div id="overlay"></div>
 <header>
     <div class="alignwide">
-        <div class="logo">Logotyp</div>
+        <a href="/" class="logo">
+            <img src="<?php bloginfo('template_url'); ?>/assets/img/uxwebb-logo.svg" alt="ux webb logo">
+        </a>
         <nav id="main-nav">
             <?php wp_nav_menu( array( 'menu' => 'Sidhuvud meny' ) ); ?>
-            <button class="toggle">Toggle</button>
+            <button class="toggle">
+                <div class="line-wrapper">
+                    <div class="line top"></div>
+                    <div class="line bottom"></div>
+                </div>
+                <span>MENY</span>
+            </button>
         </nav>
     </div>
 </header>
--- loop.php
+++ loop.php
@@ -4,7 +4,7 @@
             the_post(); 
             echo "<main>";
                 echo "<article>";
-                    // echo "<h1>"; the_title(); echo "</h1>";
+                    //echo "<h1>"; the_title(); echo "</h1>";
                     the_content();
                 echo "</article>";
             echo "</main>";
