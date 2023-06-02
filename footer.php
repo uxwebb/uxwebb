010c0b772263438b05fd7da0a4ce85577c3e31a5
uxwebb <info@uxwebb.se>
2023-06-02 16:52:28 +0200

    removed text in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="alignwide">
-        <h3>footer</h3>
-        <div class="dots"></div>
+        
+
     </div>
 </footer>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/color-thief/2.3.2/color-thief.min.js"></script>
