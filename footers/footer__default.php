886eab93ee1c3a410d37fc59aa1e37c6468e9af1
uxwebb <info@uxwebb.se>
2023-08-13 10:26:11 +0200

    init full site edit

--- footers/footer__default.php
+++
@@ -1,8 +0,0 @@
-<footer id="footer__jonkopinghalkbana">
-    <div class="wrapper">
-        <div class="alignwide">
-            <a href="/" class="logo">Default</a>
-            <?php wp_nav_menu(); ?>
-        </div>
-    </div>
-</footer>
